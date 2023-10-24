5c10cec8e3b013b7b21e6fa8b7680ce8b7718eb5
OuOHoon <wls2579@gmail.com>
2023-10-24 18:55:17 +0900

    pull 전 커밋

--- hotel-reservation/src/com/nbc/hotel/model/ViewManagement.java
+++ hotel-reservation/src/com/nbc/hotel/model/ViewManagement.java
@@ -1,9 +1,12 @@
 package com.nbc.hotel.model;
 
 import java.util.Scanner;
+import java.util.UUID;
 
 public class ViewManagement {
     private static final Scanner sc = new Scanner(System.in);
 
+    public static UUID inputUUID() {
 
+    }
 }
