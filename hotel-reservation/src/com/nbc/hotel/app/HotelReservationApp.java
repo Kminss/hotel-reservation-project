b367c44f678572eb52865fe89c92a192674c468b
MinJung Kim <kminss3711@gmail.com>
2023-10-24 17:55:38 +0900

    feature: 예약 취소 기능 구현
    1. 예약 취소 선택 화면 구현
    2. 예약 취소 완료 화면 구현
    3. 예약 취소 실패 화면 구현

--- hotel-reservation/src/com/nbc/hotel/app/HotelReservationApp.java
+++ hotel-reservation/src/com/nbc/hotel/app/HotelReservationApp.java
@@ -1,7 +1,57 @@
 package com.nbc.hotel.app;
 
+import com.nbc.hotel.Util;
+import com.nbc.hotel.exception.ReservationNotFoundException;
+import com.nbc.hotel.model.Hotel;
+import com.nbc.hotel.model.Reservation;
+import com.nbc.hotel.model.Room;
+import com.nbc.hotel.model.ViewManagement;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class HotelReservationApp {
+    ViewManagement viewManagement = new ViewManagement();
+    Hotel hotel = new Hotel();
+
     public boolean start() {
         return false;
+    }
+
+
+    public void cancelReservationProcess() {
+        String reservationId = viewManagement.showCancelReservation();
+        if (reservationId.equals("0")) {
+            return; //메인메뉴? 특정 메뉴로 돌아가는 메소드
+        }
+
+        try {
+            Reservation reservation = hotel.findReservationByUUID(Util.converStringToUuid(reservationId));
+            hotel.cancelReservation(reservation);
+            viewManagement.showCancelReservationSuccess(reservation);
+        } catch (ReservationNotFoundException exception) {
+            viewManagement.showCancelReservationFailed(exception.getMessage());
+        }
+    }
+    static class Test {
+        public static void main(String[] args) {
+            HotelReservationApp app = new HotelReservationApp();
+            Reservation reservation = new Reservation();
+
+            UUID uuid = UUID.randomUUID();
+            System.out.println(uuid);
+
+            reservation.setUuid(uuid);
+            reservation.setReservationDate(LocalDateTime.now());
+            reservation.setRoom(new Room(2.0, 10000.0));
+            reservation.setCustomerName("test");
+            reservation.setCustomerPhoneNumber("testNumber");
+
+            app.hotel.setReservations(new ArrayList<>(List.of(reservation)));
+
+            app.cancelReservationProcess();
+        }
     }
 }
--- hotel-reservation/src/com/nbc/hotel/model/Reservation.java
+++ hotel-reservation/src/com/nbc/hotel/model/Reservation.java
@@ -1,4 +1,49 @@
 package com.nbc.hotel.model;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
 public class Reservation {
+    private UUID uuid;
+    private Room room;
+    private String customerName;
+    private String customerPhoneNumber;
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setCustomerPhoneNumber(String customerPhoneNumber) {
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+
+    public void setReservationDate(LocalDateTime reservationDate) {
+        this.reservationDate = reservationDate;
+    }
+
+    private LocalDateTime reservationDate;
+
+    public String getReservationInfo() {
+        return String.format("예약일: %s %n예약번호: %s %n객실 크기: %.1f %n객실 금액: %.1f %n예약자 명: %s %n예약자 휴대폰번호: %s %n",
+                reservationDate.format(DateTimeFormatter.ISO_DATE),
+                uuid,
+                room.getSize(),
+                room.getPrice(),
+                customerName,
+                customerPhoneNumber);
+
+    }
+
+    public UUID getReservationId() {
+        return uuid;
+    }
 }
