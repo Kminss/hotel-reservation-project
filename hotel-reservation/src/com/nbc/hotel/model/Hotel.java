514a46c37db4b2f333effc33b56fcd29a12a824b
MinJung Kim <kminss3711@gmail.com>
2023-10-24 16:14:42 +0900

    클래스 설계

--- hotel-reservation/src/com/nbc/hotel/model/Hotel.java
+++ hotel-reservation/src/com/nbc/hotel/model/Hotel.java
@@ -1,4 +1,5 @@
 package com.nbc.hotel.model;
 
 public class Hotel {
+
 }
---
+++ hotel-reservation/src/com/nbc/hotel/model/ViewManagement.java
@@ -0,0 +1,9 @@
+package com.nbc.hotel.model;
+
+import java.util.Scanner;
+
+public class ViewManagement {
+    private static final Scanner sc = new Scanner(System.in);
+
+
+}
