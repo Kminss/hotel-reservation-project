0d19f77e1134bd42d8b9d10fd75e4a9e4bb23388
parkjihwan-kr <mankcom3799@hufs.ac.krecho>
2023-10-25 21:45:03 +0900

    test

--- src/com/nbc/hotel/Main.java
+++ src/com/nbc/hotel/Main.java
@@ -3,13 +3,12 @@ package com.nbc.hotel;
 import com.nbc.hotel.app.HotelReservationApp;
 
 public class Main {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         boolean ended = false;
-        HotelReservationApp app = new HotelReservationApp();
-        ended = app.start();
         while (!ended) {
-            // 프로그램 메인
+            ended = new HotelReservationApp().start();
+            ended = true;
         }
 
-    }
+    }*/
 }
--- src/com/nbc/hotel/model/Role.java
+++ src/com/nbc/hotel/model/Role.java
@@ -1,13 +1,5 @@
 package com.nbc.hotel.model;
 
 public enum Role {
-
-    ADMIN("ADMIN"),
-    CUSTOMER("CUSTOMER");
-
-    Role(String role) {
-        this.role = role;
-    }
-
-    private String role;
+	USER,ADMIN
 }
